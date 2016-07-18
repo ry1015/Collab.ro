6cbab796dcb4e4b272da43df08489087e7408560
ryan <ry1015@gmail.com>
2016-07-18 13:40:11 -0700

    Fixed Media Path - Should work for Windows

--- django/startup/settings.py
+++ django/startup/settings.py
@@ -125,4 +125,5 @@ PROJECT_DIR=os.path.dirname(__file__)
 STATIC_URL = '/static/'
 STATIC_ROOT= '/var/www/django/static'
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/django/media'
+# MEDIA_ROOT = '/var/www/django/media'
+MEDIA_ROOT = BASE_DIR + "/main/media"
