660cc003fa30eab8c06085d9408af2cdfd41ce95
Ry <ry1015@gmail.com>
2016-07-21 16:08:05 -0700

    Added track comment icon

--- django/main/static/js/home/track-list.js
+++ django/main/static/js/home/track-list.js
@@ -1,5 +1,20 @@
 var TRACK_LIST_DIV_ID = "track-list-div";
 var TRACK_LIST_TABLE_ID = "track-list-table";
+var COMMENT_CLASS = "track-comment";
+
+function pauseEventListener(){
+    var audio_list = document.getElementsByTagName("audio");
+    for (var i of audio_list){
+        i.addEventListener('play', pauseOther, false);
+    }
+}
+
+function commentsEventListener(){
+    var track_comments = document.getElementsByClassName("track-comment");
+    for (var i of track_comments){
+        console.log(i.parentNode.previousSibling.childNodes[0].src);
+    }
+}
 
 function createTrackList(parent_node){
     console.log("CREATING TRACK LIST");
@@ -19,20 +34,24 @@ function createUserTrackList(parent_node){
 
     for (filename in track_list){
         var row = track_list_table.insertRow(track_list_table.rows.length);
-        
-        var track = "<audio controls>";
-        track += "<source src=media/" + track_list[filename] + " type=audio/mpeg>";
-        track += "</audio>";
+        var track = new Audio("media/" + track_list[filename]);
+        track.controls = true;
+
+
         var cell = row.insertCell(0);
-        cell.innerHTML = track;
+        cell.appendChild(track);
+
+        var comment = document.createElement("img");
+        comment.setAttribute("class", COMMENT_CLASS);
+        comment.src = "media/comment.ico";
+        cell = row.insertCell(1);
+        cell.appendChild(comment);
     }
 
     track_list_div.appendChild(track_list_table);
     parent_node.insertBefore(track_list_div, parent_node.firstChild);
-    var audio = document.getElementsByTagName("audio");
-    for (var i of audio){
-        i.addEventListener('play', pauseOther, false);
-    }
+    pauseEventListener();
+    commentsEventListener();
 }
 
 function pauseOther(){
