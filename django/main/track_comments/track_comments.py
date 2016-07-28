9af7a7e439fe96e145db70afa12ea99dfcdecaca
Ry <ry1015@gmail.com>
2016-07-28 16:44:30 -0700

    Added new file track_comments.py

---
+++ django/main/track_comments/track_comments.py
@@ -0,0 +1,53 @@
+from django.shortcuts import render, render_to_response
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from main.serializer import UserSerializer, UserProfileSerializer, ContactInformationSerializer
+from main.models import UserProfile, UserCategory, SocialNetwork, ContactInformation, Music, TrackComment
+import json
+from startup.settings import MEDIA_ROOT
+import os
+
+# Gets all track comments
+@api_view(['GET'])
+def get_track_comments(request, format=None):
+    selected_track = request.GET.get("filename")
+
+    track_num, username, track_name = selected_track.split("_")
+    comments = []
+
+    # Get User
+    try:
+        user = User.objects.get(username=username)
+    except:
+        Response("User Does Not Exist", status=status.HTTP_400_BAD_REQUEST)
+
+    # Get Music
+    try:
+        music = Music.objects.get(userID=user, filename=selected_track)
+    except:
+        Response("No Music Exists.", status=status.HTTP_400_BAD_REQUEST)
+
+    # Get Track Comments
+    try:
+        comments = TrackComment.objects.filter(musicID=music)
+    except:
+        Response("No Track Comments.", status=status.HTTP_400_BAD_REQUEST)
+
+    print ("COMMENTS")
+    print (comments)
+    print (len(comments))
+    data = []
+    if (len(comments) > 0):
+        for comment in comments:
+            tmp = {}
+            tmp["comment"] = comment.comments
+            tmp["recipient"] = UserSerializer(comment.recipient).data
+            tmp["sender"] = UserSerializer(comment.sender).data
+            data.append(tmp)
+    else:
+        print ("NO COMMENTS")
+    data.append({"filename": selected_track})
+    return Response(data, status=status.HTTP_200_OK)
