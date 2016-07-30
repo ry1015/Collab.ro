93f079d989deac19d1933b1fe4682616f7d2bbbe
Sean Vail <seanv554@gmail.com>
2016-07-30 16:16:20 -0700

    Fixed depricated import in projects.py

--- django/main/projects/projects.py
+++ django/main/projects/projects.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
-from main.serializer import UserSerializer, UserProfileSerializer, ContactInformationSerializer, TrackCommentSerializer, ProjectSerializer
+from main.serializer import UserSerializer, UserProfileSerializer, ContactInformationSerializer, TrackCommentSerializer
 from main.models import UserProfile, UserCategory, SocialNetwork, ContactInformation, Music, TrackComment, Project
 import json
 
