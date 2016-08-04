9134ec80d72bb7c76d7691fe54671170e4c461d5
amle92 <andyle1992@yahoo.com>
2016-08-04 13:07:43 -0700

    Stems can now be Uploaded

--- django/main/admin.py
+++ django/main/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from . models import Music, Account, UserProfile, UserCategory, SocialNetwork, ContactInformation, TrackComment, Project
+from . models import Music, Account, UserProfile, UserCategory, SocialNetwork, ContactInformation, TrackComment, Project, Stem
 
 # Register your models here.
-models = {Music, Account, UserProfile, UserCategory, SocialNetwork, ContactInformation, TrackComment, Project}
+models = {Music, Account, UserProfile, UserCategory, SocialNetwork, ContactInformation, TrackComment, Project, Stem}
 admin.site.register(models)
---
+++ django/main/stems/stems.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, render_to_response
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from main.serializer import UserSerializer, UserProfileSerializer, ContactInformationSerializer, TrackCommentSerializer
+from main.models import UserProfile, UserCategory, SocialNetwork, ContactInformation, Music, TrackComment, Project, Stem
+import json
+
+@api_view(['POST'])
+def upload_stem(request, format=None):
+    print("Uploading Stem")
+    username = request.POST.get("username")
+    stem_name = request.POST.get("stem_name")
+    category = request.POST.get("category")
+    filename = request.FILES("file")
+	
+	try:
+        user = User.objects.get(username=username)
+    except:
+        return Response("Upload Stem Error. Username Does Not Exist.")
+    try:
+        project = Project.objects.get(userID=user)
+    except:
+        return Response("Upload Stem Error. Project Does Not Exist.")
+    try:
+        stem = Stems.object.create(userID=user, projectID=project, title=stem_name, category=category, filename=filename)
+    except:
+        return Response("Upload Stem Error. Cannot upload stem.", status=status.HTTP_400_BAD_REQUEST)
+    stem.save()
+    return Response({}, status=status.HTTP_200_OK)
