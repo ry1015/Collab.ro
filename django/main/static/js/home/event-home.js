63b5c952670a3b399510031715e41f1988350c32
Sean Vail <seanv554@gmail.com>
2016-08-15 23:44:28 -0700

    new project table updated

--- django/main/static/js/home/event-home.js
+++ django/main/static/js/home/event-home.js
@@ -3,8 +3,6 @@ function newProjectEvent(){
     console.log("ADD NEW PROJECT!");
     var project_table = document.getElementById(PROJECT_TABLE_ID).getElementsByTagName('tbody')[0];
      
-     
-
     // row = new_project_table.insertRow(new_project_table.rows.length);
     // cell = row.insertCell(0);
     // cell.innerHTML = "<input placeholder='UPLOAD TRACK'>";
@@ -14,24 +12,37 @@ function newProjectEvent(){
     // cell.innerHTML = "<input placeholder='UPLOAD STEM'>";
     
      var row = project_table.insertRow(0);
+     row.id = NEW_PROJECT_ROW_ID;
      var cell = row.insertCell(0);
+     var new_project_table = document.createElement("table");
+     
+     new_project_table.className = NEW_PROJECT_TABLE_ID;
+     cell.appendChild(new_project_table);
+     
+     row = new_project_table.insertRow(0);
+     cell = row.insertCell(0);
      cell.innerHTML = "<input id='project_name_field' value='Test Project'>";
-	 cell = row.insertCell(1);
+     row = new_project_table.insertRow(1);
+     cell = row.insertCell(0);
 	 cell.innerHTML = "STEMS:<input id='stem_title' placeholder='ENTER TITLE'>";
-	 cell = row.insertCell(2);
+	 row = new_project_table.insertRow(2);
+     cell = row.insertCell(0);
 	 cell.innerHTML = "Category:<select id='stem_category'> <option value='drums'>Drums" +
 	                           "<option value='guitar'>Guitar" +
 						       "<option value='producer'>Producer" +
 							   "<option value='vocal'>Vocal";
-	 cell = row.insertCell(3);
+	 row = new_project_table.insertRow(3);
+     cell = row.insertCell(0);
 	 cell.innerHTML = "<input id='stem_upload' type='file'>";
-	 cell = row.insertCell(4);
+	 row = new_project_table.insertRow(4);
+     cell = row.insertCell(0);
 	 cell.innerHTML = "<button id='upload_button'>UPLOAD</button";
 	
-//     row = project_table.insertRow(project_table.rows.length);
+//     row = new_project_table.insertRow(project_table.rows.length);
 //     cell = row.insertCell(5);
 
-     cell = row.insertCell(5);
+	 row = new_project_table.insertRow(5);
+     cell = row.insertCell(0);
      cell.innerHTML = "<button id='save_button'>SAVE</button>";
 
     var parent_projects_table = document.getElementById("project_table");
@@ -48,6 +59,8 @@ function saveProjectEvent(){
 	var processProject = function(result)
 	{
         console.log("Project saved with id: " + result);
+        var element = document.getElementById(NEW_PROJECT_ROW_ID).outerHTML = "";
+        delete element;
         refreshProjects();
 	}
 	
