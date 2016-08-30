93b482eeff0ca1113dce1ebf4d99dd10c21e3d47
amle92 <andyle1992@yahoo.com>
2016-08-29 23:14:42 -0700

    add status field to Project, Track, and Stem models.

--- django/main/models.py
+++ django/main/models.py
@@ -108,7 +108,7 @@ class Stem(models.Model):
     category = models.CharField(max_length=200)
     filename = models.FileField(max_length=200, blank=True)
     upload_date = models.DateTimeField(default=timezone.now)
-    status = models.CharField(max_length=7, default="public")
+    status = models.CharField(max_length=7, default="private")
 	
     def __str__(self):
         preview = "[ID: " + str(self.id) + "]" + "[" + str(self.projectID) + "](" + str(self.userID) + ")" + self.title
