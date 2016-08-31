cc9cd3b95c0e606d8190563fb9f75ad25e00f542
Ry <ry1015@gmail.com>
2016-08-31 10:16:49 -0700

    Fixed Admin Model Layout

--- django/main/admin.py
+++ django/main/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from . models import Music, Account, UserProfile, UserCategory, SocialNetwork, ContactInformation, TrackComment, Project, Stem, Track
 
 # Register your models here.
-models = {Music, Account, UserProfile, UserCategory, SocialNetwork, ContactInformation, TrackComment, Project, Stem, Track}
+models = {
+    Music, 
+    Account, 
+    UserProfile, 
+    UserCategory, 
+    SocialNetwork, 
+    ContactInformation, 
+    TrackComment, 
+    Project, 
+    Stem, 
+    Track
+    }
+    
 admin.site.register(models)
