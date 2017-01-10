9ccacfffef32aade23cc98a70579c9eaf234e91a
Ry <ry1015@gmail.com>
2017-01-10 13:48:59 -0800

    Fixed Authentication Error

--- django/startup/settings.py
+++ django/startup/settings.py
@@ -52,9 +52,6 @@ MIDDLEWARE_CLASSES = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     )
